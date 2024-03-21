7fd50882f75338757a1e75fdf33ef9ccd1a5601a
itsbayahmed <ahmed-bay@live.fr>
2024-03-21 07:49:36 +0000

    A script that prints the number of arguments passed

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -7,10 +7,10 @@ if __name__ == '__main__':
         print("0 arguments.")
 
     elif (len(sys.argv) == 2):
-        print("{:d} argument: ".format(len(sys.argv) - 1))
+        print("{:d} argument:".format(len(sys.argv) - 1))
 
     else:
-        print("{:d} arguments: ".format(len(sys.argv) - 1))
+        print("{:d} arguments:".format(len(sys.argv) - 1))
 
     for n in range(1, len(sys.argv)):
         print("{:d}: {}".format(n, sys.argv[n]))
